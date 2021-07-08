0aaac46adc20e9818ae55775998146263e2e1589
naman2807 <namanagarwaldbs438@gmail.com>
2021-07-08 10:26:02 +0530

    Initial commit

---
+++ src/main/kotlin/agarwal/naman/springlearning/springcourseinkotlin/SpringCourseInKotlinApplication.kt
@@ -0,0 +1,11 @@
+package agarwal.naman.springlearning.springcourseinkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCourseInKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringCourseInKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/agarwal/naman/springlearning/springcourseinkotlin/SpringcourseinkotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package agarwal.naman.springlearning.springcourseinkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringcourseinkotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
