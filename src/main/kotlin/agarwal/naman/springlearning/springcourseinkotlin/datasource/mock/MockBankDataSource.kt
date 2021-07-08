da2bee776cc53dfbcb200cf1ad48d51b1f5396f3
naman2807 <namanagarwaldbs438@gmail.com>
2021-07-08 10:53:04 +0530

    Extended the MockBankDataSource.kt to BankDataSource interface

--- src/main/kotlin/agarwal/naman/springlearning/springcourseinkotlin/datasource/mock/MockBankDataSource.kt
+++ src/main/kotlin/agarwal/naman/springlearning/springcourseinkotlin/datasource/mock/MockBankDataSource.kt
@@ -1,5 +1,7 @@
 package agarwal.naman.springlearning.springcourseinkotlin.datasource.mock;
 
+import agarwal.naman.springlearning.springcourseinkotlin.datasource.BankDataSource
+
 /**
 Created By: Naman Agarwal
 User ID: naman2807
@@ -8,5 +10,5 @@ Project Name: springcourseinkotlin
 Date: 08-07-2021
  */
 
-class MockBankDataSource {
+class MockBankDataSource: BankDataSource {
 }
