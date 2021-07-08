0e67cac78fa8f64b68089e132e05fc36397a26db
naman2807 <namanagarwaldbs438@gmail.com>
2021-07-08 11:01:38 +0530

    Created MockBankDataSourceTest.kt class for testing purpose

--- src/main/kotlin/agarwal/naman/springlearning/springcourseinkotlin/datasource/mock/MockBankDataSource.kt
+++ src/main/kotlin/agarwal/naman/springlearning/springcourseinkotlin/datasource/mock/MockBankDataSource.kt
@@ -19,3 +19,11 @@ class MockBankDataSource: BankDataSource {
         TODO("Not yet implemented")
     }
 }
+
+//@Repository marks MockBankDataSource class as Spring Boot bean. So this adds it to application context
+//and it tells spring to initialize this bean/object at runtime and it also tells spring that it is not
+//just any bean/object, it's in fact a repository which has few implications later on, for instance when
+//testing things.
+
+//In other words, marking class as @Repository means that class is responsible for retrieving data, deleting
+//data, updating data etc.
---
+++ src/test/kotlin/agarwal/naman/springlearning/springcourseinkotlin/datasource/mock/MockBankDataSourceTest.kt
@@ -0,0 +1,7 @@
+package agarwal.naman.springlearning.springcourseinkotlin.datasource.mock
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class MockBankDataSourceTest{
+
+}
