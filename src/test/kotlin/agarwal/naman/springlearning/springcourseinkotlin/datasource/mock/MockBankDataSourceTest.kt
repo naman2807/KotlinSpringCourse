a68381e958c2c0873107da834dccad5083538227
naman2807 <namanagarwaldbs438@gmail.com>
2021-07-08 11:08:13 +0530

    Created test for MockBankDataSourceTest.kt

--- src/test/kotlin/agarwal/naman/springlearning/springcourseinkotlin/datasource/mock/MockBankDataSourceTest.kt
+++ src/test/kotlin/agarwal/naman/springlearning/springcourseinkotlin/datasource/mock/MockBankDataSourceTest.kt
@@ -1,7 +1,18 @@
 package agarwal.naman.springlearning.springcourseinkotlin.datasource.mock
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
 internal class MockBankDataSourceTest{
-
+    @Test
+    fun `should `(){
+         // given
+         
+         
+         // when
+         
+         
+         // then
+         
+    }
 }
