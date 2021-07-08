db009cb70c3654725369f1770b09ad3658dacaea
naman2807 <namanagarwaldbs438@gmail.com>
2021-07-08 10:28:21 +0530

    Created Bank.kt class

---
+++ src/main/kotlin/agarwal/naman/springlearning/springcourseinkotlin/model/Bank.kt
@@ -0,0 +1,12 @@
+package agarwal.naman.springlearning.springcourseinkotlin.model;
+
+/**
+Created By: Naman Agarwal
+User ID: naman2807
+Package Name: agarwal.naman.springlearning.springcourseinkotlin.model
+Project Name: springcourseinkotlin
+Date: 08-07-2021
+ */
+
+class Bank {
+}
