ddbe2aa8ee5f01f8d1ac15be084e96a5daad3dc5
naman2807 <namanagarwaldbs438@gmail.com>
2021-07-20 11:05:27 +0530

    Created test in BankServiceTest.kt

--- src/test/kotlin/agarwal/naman/springlearning/springcourseinkotlin/service/BankServiceTest.kt
+++ src/test/kotlin/agarwal/naman/springlearning/springcourseinkotlin/service/BankServiceTest.kt
@@ -3,10 +3,23 @@ package agarwal.naman.springlearning.springcourseinkotlin.service
 import agarwal.naman.springlearning.springcourseinkotlin.datasource.BankDataSource
 import agarwal.naman.springlearning.springcourseinkotlin.datasource.mock.MockBankDataSource
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
 internal class BankServiceTest{
     private val bankDataSource : BankDataSource = mockk()
 
     private val bankService = BankService(bankDataSource = bankDataSource)
+    
+    @Test
+    fun `should `(){
+         // given
+         
+         
+         // when
+         
+         
+         // then
+         
+    }
 
 }
