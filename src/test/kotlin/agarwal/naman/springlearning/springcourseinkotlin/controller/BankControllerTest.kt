26c1ce531c53820b71164976b11b6a83cbaa1cea
naman2807 <namanagarwaldbs438@gmail.com>
2021-07-20 14:42:38 +0530

    Added content to test

--- src/test/kotlin/agarwal/naman/springlearning/springcourseinkotlin/controller/BankControllerTest.kt
+++ src/test/kotlin/agarwal/naman/springlearning/springcourseinkotlin/controller/BankControllerTest.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 
@@ -22,7 +23,8 @@ internal class BankControllerTest{
             .andDo { print() }
             .andExpect {
                 status { isOk() }
-                jsonPath("${0}.accountNumber") {value("abcdef")}
+                content { contentType(MediaType.APPLICATION_JSON) }
+                jsonPath("${0}.accountNumber") {value("123")}
             }
     }
 }
