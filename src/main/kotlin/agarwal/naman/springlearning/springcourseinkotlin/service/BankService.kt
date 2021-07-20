305160011bdfd4a8294414ee154e13e825ae1611
naman2807 <namanagarwaldbs438@gmail.com>
2021-07-20 10:43:38 +0530

    Created BankService.kt class

---
+++ src/main/kotlin/agarwal/naman/springlearning/springcourseinkotlin/service/BankService.kt
@@ -0,0 +1,12 @@
+package agarwal.naman.springlearning.springcourseinkotlin.service;
+
+/**
+Created By: Naman Agarwal
+User ID: naman2807
+Package Name: agarwal.naman.springlearning.springcourseinkotlin.service
+Project Name: springcourseinkotlin
+Date: 20-07-2021
+ */
+
+class BankService {
+}
