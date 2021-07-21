4bedd271ac7112668891d5affa58f21dc6574e37
naman2807 <namanagarwaldbs438@gmail.com>
2021-07-21 09:50:44 +0530

    called status block

--- src/test/kotlin/agarwal/naman/springlearning/springcourseinkotlin/controller/BankControllerTest.kt
+++ src/test/kotlin/agarwal/naman/springlearning/springcourseinkotlin/controller/BankControllerTest.kt
@@ -66,7 +66,7 @@ internal class BankControllerTest{
             mockMvc.get("/api/banks/get/$accountNumber")
                 .andDo { print() }
                 .andExpect {
-                    status { isOk() }
+                    status { isNotFound() }
                 }
              
         }
