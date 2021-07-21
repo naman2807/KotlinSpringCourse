3dfd41f1b97373d0f9b2dcfbce6f7d3e73009912
naman2807 <namanagarwaldbs438@gmail.com>
2021-07-21 09:57:01 +0530

    Annotated handleNotFound() method in BankController.kt to handle exception

--- src/main/kotlin/agarwal/naman/springlearning/springcourseinkotlin/controller/BankController.kt
+++ src/main/kotlin/agarwal/naman/springlearning/springcourseinkotlin/controller/BankController.kt
@@ -4,10 +4,7 @@ import agarwal.naman.springlearning.springcourseinkotlin.model.Bank
 import agarwal.naman.springlearning.springcourseinkotlin.service.BankService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 /**
 Created By: Naman Agarwal
@@ -21,6 +18,7 @@ Date: 20-07-2021
 @RequestMapping("/api/banks")
 class BankController(private val service: BankService) {
 
+    @ExceptionHandler(NoSuchElementException::class)
     fun handleNotFound(e: NoSuchElementException): ResponseEntity<String> =
         ResponseEntity(e.message, HttpStatus.NOT_FOUND)
 
