8fcd900ab85151452b09b0cd81adbd6eca69fdd3
naman2807 <namanagarwaldbs438@gmail.com>
2021-07-22 11:17:37 +0530

    Created updateBank() method in BankDataSource.kt

--- src/main/kotlin/agarwal/naman/springlearning/springcourseinkotlin/datasource/BankDataSource.kt
+++ src/main/kotlin/agarwal/naman/springlearning/springcourseinkotlin/datasource/BankDataSource.kt
@@ -15,4 +15,5 @@ interface BankDataSource {
     fun retrieveBanks() : Collection<Bank>
     fun retrieveBank(accountNumber: String): Bank
     fun addBank(bank: Bank): Bank
+    fun updateBank(bank: Bank): Bank
 }
