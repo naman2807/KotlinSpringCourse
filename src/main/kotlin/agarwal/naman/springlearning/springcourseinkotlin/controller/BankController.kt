9c4f91b15490e4a09b0e1d15513dcd579e71439e
naman2807 <namanagarwaldbs438@gmail.com>
2021-07-22 10:35:01 +0530

    Created handleBadRequest() method

--- src/main/kotlin/agarwal/naman/springlearning/springcourseinkotlin/controller/BankController.kt
+++ src/main/kotlin/agarwal/naman/springlearning/springcourseinkotlin/controller/BankController.kt
@@ -5,6 +5,7 @@ import agarwal.naman.springlearning.springcourseinkotlin.service.BankService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.lang.IllegalArgumentException
 
 /**
 Created By: Naman Agarwal
@@ -21,6 +22,9 @@ class BankController(private val service: BankService) {
     @ExceptionHandler(NoSuchElementException::class)
     fun handleNotFound(e: NoSuchElementException): ResponseEntity<String> =
         ResponseEntity(e.message, HttpStatus.NOT_FOUND)
+
+    fun handleBadRequest(e: IllegalArgumentException): ResponseEntity<String> =
+
 
     @GetMapping("/get")
     fun getBanks(): Collection<Bank>{
